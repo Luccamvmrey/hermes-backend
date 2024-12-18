ea173ee41321a1ce39ebe9a12689777bbee98e46
Axcell Oliveira <axcellolliveira@gmail.com>
2024-12-18 16:29:55 -0300

    Correção de sub-contas

--- src/support-tables/sub-contas/sub-contas.service.ts
+++ src/support-tables/sub-contas/sub-contas.service.ts
@@ -1,45 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { CreateSubContaDto } from './dto/create-sub-conta.dto';
-import { UpdateSubContaDto } from './dto/update-sub-conta.dto';
-import { DatabaseService } from '../../database/database.service';
+import {Injectable} from '@nestjs/common';
+import {CreateSubContaDto} from './dto/create-sub-conta.dto';
+import {UpdateSubContaDto} from './dto/update-sub-conta.dto';
+import {DatabaseService} from '../../database/database.service';
 
 @Injectable()
 export class SubContasService {
-  constructor(private readonly databaseService: DatabaseService) {}
+    constructor(private readonly databaseService: DatabaseService) {
+    }
 
-  create(data: CreateSubContaDto) {
-    console.log(data);
-    return this.databaseService.subConta.create({
-      data,
-    });
-  }
+    create(data: CreateSubContaDto) {
+        console.log(data);
+        return this.databaseService.subConta.create({
+            data,
+        });
+    }
 
-  findAll() {
-    return this.databaseService.subConta.findMany();
-  }
+    findAll() {
+        return this.databaseService.subConta.findMany({
+            include: {
+                ContaContabil: true,
+            }
+        });
+    }
 
-  findOne(id: number) {
-    return this.databaseService.subConta.findUnique({
-      where: {
-        id,
-      },
-    });
-  }
+    findOne(id: number) {
+        return this.databaseService.subConta.findUnique({
+            where: {
+                id,
+            },
+            include: {
+                ContaContabil: true,
+            }
+        });
+    }
 
-  update(id: number, data: UpdateSubContaDto) {
-    return this.databaseService.subConta.update({
-      where: {
-        id,
-      },
-      data,
-    });
-  }
+    update(id: number, data: UpdateSubContaDto) {
+        return this.databaseService.subConta.update({
+            where: {
+                id,
+            },
+            data,
+        });
+    }
 
-  remove(id: number) {
-    return this.databaseService.subConta.delete({
-      where: {
-        id,
-      },
-    });
-  }
+    remove(id: number) {
+        return this.databaseService.subConta.delete({
+            where: {
+                id,
+            },
+        });
+    }
 }
