0a8e378cee69f07b63258f88264d2499d3c3246b
Axcell Oliveira <axcellolliveira@gmail.com>
2024-12-19 18:06:25 -0300

    Centro de Custo

--- src/financial/contas-pagar/dto/create-contas-pagar.dto.ts
+++ src/financial/contas-pagar/dto/create-contas-pagar.dto.ts
@@ -1,91 +1,96 @@
-import { Prisma } from '@prisma/client';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import {Prisma} from '@prisma/client';
+import {ApiProperty} from '@nestjs/swagger';
+import {IsNotEmpty, IsNumber, IsString} from "class-validator";
 
 export class CreateContasPagarDto {
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  nfe: number;
+    @IsNumber()
+    @IsNotEmpty()
+    @ApiProperty()
+    nfe: number;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  serie: number;
+    @IsNumber()
+    @IsNotEmpty()
+    @ApiProperty()
+    serie: number;
 
-  @ApiProperty({ required: false })
-  criadoEm?: Date | string;
+    @ApiProperty({required: false})
+    criadoEm?: Date | string;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  doc: string;
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    doc: string;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @ApiProperty()
-  valor: number | string;
+    @IsNumber()
+    @IsNotEmpty()
+    @ApiProperty()
+    valor: number | string;
 
-  @IsNotEmpty()
-  @ApiProperty()
-  dataVencimento: Date | string;
+    @IsNotEmpty()
+    @ApiProperty()
+    dataVencimento: Date | string;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  descricao: string;
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    descricao: string;
 
-  @ApiProperty({ required: false })
-  percentualJuros: number | string;
+    @ApiProperty({required: false})
+    percentualJuros: number | string;
 
-  @ApiProperty({ required: false })
-  percentualDesconto: number | string;
+    @ApiProperty({required: false})
+    percentualDesconto: number | string;
 
-  @ApiProperty({ required: false })
-  valorPago: number | string;
+    @ApiProperty({required: false})
+    valorPago: number | string;
 
-  @ApiProperty()
-  situacao: string;
+    @ApiProperty()
+    situacao: string;
 
-  @ApiProperty({ required: false })
-  flag: boolean;
+    @ApiProperty({required: false})
+    flag: boolean;
 
-  @ApiProperty({ required: false })
-  codBarras: string;
+    @ApiProperty({required: false})
+    codBarras: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  especie: string;
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    especie: string;
 
-  @IsNotEmpty()
-  @ApiProperty()
-  dataEmissao: Date | string;
+    @IsNotEmpty()
+    @ApiProperty()
+    dataEmissao: Date | string;
 
-  @IsNotEmpty()
-  @ApiProperty()
-  Empresa: Prisma.EmpresaCreateNestedOneWithoutContaPagarInput;
+    @IsNotEmpty()
+    @ApiProperty()
+    Empresa: Prisma.EmpresaCreateNestedOneWithoutContaPagarInput;
 
-  @IsNotEmpty()
-  @ApiProperty()
-  FormaPagamento: Prisma.FormaPagamentoCreateNestedOneWithoutContaPagarInput;
+    @IsNotEmpty()
+    @ApiProperty()
+    FormaPagamento: Prisma.FormaPagamentoCreateNestedOneWithoutContaPagarInput;
 
-  @IsNotEmpty()
-  @ApiProperty()
-  Pessoa: Prisma.PessoaCreateNestedOneWithoutContaPagarInput;
+    @IsNotEmpty()
+    @ApiProperty()
+    Pessoa: Prisma.PessoaCreateNestedOneWithoutContaPagarInput;
 
-  @IsNotEmpty()
-  @ApiProperty()
-  Usuario: Prisma.UsuarioCreateNestedOneWithoutContaPagarInput;
+    @IsNotEmpty()
+    @ApiProperty()
+    Usuario: Prisma.UsuarioCreateNestedOneWithoutContaPagarInput;
 
-  @IsNotEmpty()
-  @ApiProperty()
-  ContaContabil: Prisma.ContaContabilCreateNestedOneWithoutContaPagarInput
+    @IsNotEmpty()
+    @ApiProperty()
+    ContaContabil: Prisma.ContaContabilCreateNestedOneWithoutContaPagarInput
 
-  @IsNotEmpty()
-  @ApiProperty()
-  SubConta: Prisma.SubContaCreateNestedOneWithoutContaPagarInput
+    @IsNotEmpty()
+    @ApiProperty()
+    CentroCusto: Prisma.CentroCustoCreateNestedOneWithoutContaPagarInput
 
-  @ApiProperty({ required: false })
-  Pagamento?: Prisma.PagamentoCreateNestedManyWithoutContaPagarInput;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    SubConta: Prisma.SubContaCreateNestedOneWithoutContaPagarInput
+
+    @ApiProperty({required: false})
+    Pagamento?: Prisma.PagamentoCreateNestedManyWithoutContaPagarInput;
 }
