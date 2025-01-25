f988e7bbd74e1eb44e6d523cdeb5381c61f5b207
luccamvmrey <luccamvmachadorey@gmail.com>
2025-01-25 13:16:09 -0300

    Added: modulo permissões

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,24 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {DatabaseModule} from './database/database.module';
-import {BancosModule} from './support-tables/bancos/bancos.module';
-import {ContasModule} from './support-tables/contas/contas.module';
-import {EmpresasModule} from './support-tables/empresas/empresas.module';
-import {PessoasModule} from './support-tables/pessoas/pessoas.module';
-import {FormasPagamentoModule} from './support-tables/formas-pagamento/formas-pagamento.module';
-import {GerenciasModule} from './support-tables/gerencias/gerencias.module';
-import {UsuariosModule} from './user/usuarios/usuarios.module';
-import {AuthModule} from './user/auth/auth.module';
-import {EmpresaUsuarioModule} from './support-tables/empresa-usuario/empresa-usuario.module';
-import {ContasPagarModule} from './financial/contas-pagar/contas-pagar.module';
-import {PagamentosModule} from './financial/pagamentos/pagamentos.module';
-import {ContasContabeisModule} from './support-tables/contas-contabeis/contas-contabeis.module';
-import {SubContasModule} from './support-tables/sub-contas/sub-contas.module';
-import {MinioClientModule} from './storage/minio/minio-client.module';
-import {ArquivosModule} from './storage/arquivos/arquivos.module';
-import {CentroCustoModule} from "./support-tables/centro-custo/centro-custo.module";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { BancosModule } from './support-tables/bancos/bancos.module';
+import { ContasModule } from './support-tables/contas/contas.module';
+import { EmpresasModule } from './support-tables/empresas/empresas.module';
+import { PessoasModule } from './support-tables/pessoas/pessoas.module';
+import { FormasPagamentoModule } from './support-tables/formas-pagamento/formas-pagamento.module';
+import { GerenciasModule } from './support-tables/gerencias/gerencias.module';
+import { UsuariosModule } from './user/usuarios/usuarios.module';
+import { AuthModule } from './user/auth/auth.module';
+import { EmpresaUsuarioModule } from './support-tables/empresa-usuario/empresa-usuario.module';
+import { ContasPagarModule } from './financial/contas-pagar/contas-pagar.module';
+import { PagamentosModule } from './financial/pagamentos/pagamentos.module';
+import { ContasContabeisModule } from './support-tables/contas-contabeis/contas-contabeis.module';
+import { SubContasModule } from './support-tables/sub-contas/sub-contas.module';
+import { MinioClientModule } from './storage/minio/minio-client.module';
+import { ArquivosModule } from './storage/arquivos/arquivos.module';
+import { CentroCustoModule } from "./support-tables/centro-custo/centro-custo.module";
+import { PermissoesModule } from './user/permissoes/permissoes.module';
 
 @Module({
     imports: [
@@ -37,7 +38,8 @@ import {CentroCustoModule} from "./support-tables/centro-custo/centro-custo.modu
         SubContasModule,
         MinioClientModule,
         ArquivosModule,
-        CentroCustoModule
+        CentroCustoModule,
+        PermissoesModule
     ],
     controllers: [AppController],
     providers: [AppService],
