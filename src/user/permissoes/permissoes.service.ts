0d4fc520030183f87b0f17760305b570e029f2d5
luccamvmrey <luccamvmachadorey@gmail.com>
2025-01-26 22:43:49 -0300

    Refactor

--- src/user/permissoes/permissoes.service.ts
+++ src/user/permissoes/permissoes.service.ts
@@ -6,13 +6,16 @@ import { UpdatePermissoesDto } from './dto/update-permissoes.dto';
 export class PermissoesService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async updatePermissoes(userId: number, { pages }: UpdatePermissoesDto) {
+  async updatePermissoes(
+    userId: number,
+    updatePermissoesDto: UpdatePermissoesDto,
+  ) {
     await this.databaseService.permissoesUsuario.deleteMany({
       where: { idUsuario: userId },
     });
 
     return this.databaseService.permissoesUsuario.createMany({
-      data: pages.map((page) => {
+      data: updatePermissoesDto.pages.map((page) => {
         return { page, idUsuario: userId };
       }),
     });
